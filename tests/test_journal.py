3d7feebb8bbb172858b53a20c0439f6833cbed1f
Jason Tyler <jmtyler@gmail.com>
2015-08-17 23:14:00 -0700

    BDD tests done

--- tests/test_journal.py
+++ tests/test_journal.py
@@ -2,83 +2,20 @@
 from __future__ import unicode_literals
 import os
 import pytest
-from sqlalchemy import create_engine
 from sqlalchemy.exc import IntegrityError
-from pyramid import testing
-from cryptacular.bcrypt import BCRYPTPasswordManager
+import journal
 
-TEST_DATABASE_URL = os.environ.get(
-    'DATABASE_URL',
-    'postgresql://jason@localhost:5432/test-learning-journal'
-)
 
 INPUT_BTN = '<input type="submit" name="Submit" value="new">'
 
 
-os.environ['DATABASE_URL'] = TEST_DATABASE_URL
-os.environ['TESTING'] = "True"
+def login_helper(username, password, app):
+    """encapsulate app login for reuse in tests
 
-import journal
-
-
-@pytest.fixture(scope='session')
-def connection(request):
-    engine = create_engine(TEST_DATABASE_URL)
-    journal.Base.metadata.create_all(engine)
-    connection = engine.connect()
-    journal.DBSession.registry.clear()
-    journal.DBSession.configure(bind=connection)
-    journal.Base.metadata.bind = engine
-    request.addfinalizer(journal.Base.metadata.drop_all)
-    return connection
-
-
-@pytest.fixture()
-def db_session(request, connection):
-    from transaction import abort
-    trans = connection.begin()
-    request.addfinalizer(trans.rollback)
-    request.addfinalizer(abort)
-    from journal import DBSession
-    return DBSession
-
-
-@pytest.fixture
-def app(db_session):
-    from journal import main
-    from webtest import TestApp
-    app = main()
-    # main is just a factory that builds/returns configured wsgi apps
-    return TestApp(app)
-
-
-@pytest.fixture()
-def entry(db_session):
-    entry = journal.Entry.write(
-        title='Test Title',
-        body_text='Test Entry Text',
-        session=db_session
-    )
-    db_session.flush()
-    return entry
-
-
-@pytest.fixture(scope='function')
-def auth_req(request):
-    manager = BCRYPTPasswordManager()
-    settings = {
-        'auth.username': 'admin',
-        'auth.password': manager.encode('secret'),
-    }
-    testing.setUp(settings=settings)
-    req = testing.DummyRequest()
-
-    def cleanup():
-        testing.tearDown()
-
-    request.addfinalizer(cleanup)
-
-    return req
+    Accept all status codes so that we can make assertions in tests
+    """
+    login_data = {'username': username, 'password': password}
+    return app.post('/login', params=login_data, status='*')
 
 
 def test_write_entry(db_session):
@@ -190,7 +127,7 @@ def test_get_add_view_not_authorized(app, auth_req):
     assert "You need to be logged in to do this." in actual
 
 
-def test_add_no_params(app):
+def test_add_no_params(app, auth_req):
     auth_req.params = {'username': 'admin', 'password': 'secret'}
     redirect = app.post('/login', params=auth_req.params)
     response = redirect.follow()
@@ -223,15 +160,6 @@ def test_do_login_missing_params(auth_req):
         auth_req.params = params
         with pytest.raises(ValueError):
             do_login(auth_req)
-
-
-def login_helper(username, password, app):
-    """encapsulate app login for reuse in tests
-
-    Accept all status codes so that we can make assertions in tests
-    """
-    login_data = {'username': username, 'password': password}
-    return app.post('/login', params=login_data, status='*')
 
 
 def test_start_as_anonymous(app):
--- tests/test_journal_bdd.py
+++ tests/test_journal_bdd.py
@@ -2,13 +2,27 @@
 from __future__ import unicode_literals
 import pytest
 from pytest_bdd import scenario, given, when, then
-# from test_journal import app, login_helper, db_session, entry, connection
-# from test_journal import TEST_DATABASE_URL
 import journal
+from test_journal import login_helper
 
 
 @scenario('views.feature', 'Viewing Permalinks')
 def test_permalink():
+    pass
+
+
+@scenario('views.feature', 'Edit an Article')
+def test_edit_article():
+    pass
+
+
+@scenario('views.feature', 'Checking Code Highlighting')
+def test_code_highlight():
+    pass
+
+
+@scenario('views.feature', 'Check Markdown Styling')
+def test_markdown():
     pass
 
 
@@ -17,7 +31,15 @@ def anon_user():
     pass
 
 
-@given('There are is an entry')
+@given("I am an author user")
+def login(app):
+    username, password = ('admin', 'secret')
+    redirect = login_helper(username, password, app)
+    response = redirect.follow()
+    return response
+
+
+@given('There is an entry')
 def one_entry(db_session):
     entry = journal.Entry.write(
         title='Test Title',
@@ -56,22 +78,62 @@ def one_entry(db_session):
     return entry
 
 
-@when('I go to the home page')
-def home(app):
+@given('I go to the home page')
+def home(app, one_entry):
     detail = app.get('/')
     assert detail.status_code == 200
     return detail
 
 
-@then('The aricle should have a permalink')
+@given("I submit my edits")
+def edit(app, one_entry):
+    edit_data = {
+        'title': 'Hello there: edited',
+        'body_text': 'This is an edited post'
+    }
+    edit_existing = app.post('/edit/{entry_id}'.format(entry_id=one_entry.id),
+        params=edit_data, status='3*')
+    return edit_data
+
+
+@then('The article should have a permalink')
 def check_link(home, one_entry):
-    assert 'href=/detail/{entry_id}'.format(one_entry.id) in home.body
+    assert 'href="/detail/{entry_id}"'.format(entry_id=one_entry.id) in home.body
     assert one_entry.title in home.body
 
 
-@given('Requesting it should give me the article contents')
+@then('Requesting it should give me the article contents')
 def go_to_home(one_entry, app):
+    # import pdb; pdb.set_trace()
     detail = app.get('/detail/{entry_id}'.format(entry_id=one_entry.id))
     assert detail.status_code == 200
-    assert one_entry.body_text in detail.body
-    assert one_entry.title in detail.body
+    assert "Here is a small title, should be h4" in detail.text
+
+
+@then('I should see my changes')
+def check_edit(edit, app, one_entry):
+    detail = app.get('/detail/{entry_id}'.format(entry_id=one_entry.id))
+    assert edit['body_text'] in detail.body
+    assert edit['title'] in detail.body
+
+
+@then('The code text should have highlighting style')
+def check_highlighting(one_entry, app):
+    detail = app.get('/detail/{entry_id}'.format(entry_id=one_entry.id))
+    print detail.body
+    assert '<div class="codehilite">' in detail.body
+    assert '<span class="kn">' in detail.body
+    assert '<span class="p">' in detail.body
+    assert '<span class="nf">' in detail.body
+
+
+@then('The entry text should have markdown styling')
+def check_markdown(one_entry, app):
+    detail = app.get('/detail/{entry_id}'.format(entry_id=one_entry.id))
+    print detail.body
+    assert '<div class="markdown">' in detail.body
+    assert "<h1>Here is a large title, should be h1</h1>" in detail.body
+    assert "<h3>Here is a small title, should be h3</h3>" in detail.body
+    assert "<ul>" in detail.body
+    assert "<ol>" in detail.body
+    assert "<li>One</li>" in detail.body
